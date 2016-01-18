bad722dbd6ba86ce9ec0cba067faeb100c561e94
MJ <tywf91@gmail.com>
2016-01-18 18:01:35 +0800

    Initial Commit

---
+++ RNDGActivityIndicatorView/RNDGActivityIndicatorView.h
@@ -0,0 +1,19 @@
+//
+//  RNDGActivityIndicatorView.h
+//  RNDGActivityIndicatorView
+//
+//  Created by 林满佳 on 16/1/18.
+//  Copyright © 2016年 MJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RNDGActivityIndicatorView.
+FOUNDATION_EXPORT double RNDGActivityIndicatorViewVersionNumber;
+
+//! Project version string for RNDGActivityIndicatorView.
+FOUNDATION_EXPORT const unsigned char RNDGActivityIndicatorViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNDGActivityIndicatorView/PublicHeader.h>
+
+
